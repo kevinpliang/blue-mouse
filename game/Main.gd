c2d18f777f1a86d0067d76c88d453fb2f284f95e
rahil <rahil@vt.edu>
2025-05-11 00:49:05 -0700

    add main menu

--- game/Main.gd
+++ game/Main.gd
@@ -3,9 +3,15 @@ extends Node2D
 
 var MailTruck = preload("res://characters/MailTruck.tscn")
 var World = preload("res://environment/World.tscn")
+var Menu = preload("res://game/Menu.tscn")
 
 func _ready():
 	Global.main = self
+	#Global.instance_node(World, self)
+	#Global.instance_node(MailTruck, self)
+
+
+func _on_start_button_down():
 	Global.instance_node(World, self)
 	Global.instance_node(MailTruck, self)
-	
+	$Menu.queue_free()
